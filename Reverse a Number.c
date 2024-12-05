802e7ec7d1bcd19552ea5649fb21202ae2ee93dd
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2024-12-05 22:36:15 +0600

    Add files via upload

---
+++ Reverse a Number.c
@@ -0,0 +1,18 @@
+//Write a C program to reverse a given integer using a while loop
+#include<stdio.h>
+int main()
+{
+
+    int x,y=0,z;
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        z=x%10;
+        y=(y*10)+z;
+        x=x/10;
+    }
+    printf("%d is reversed",y);
+    return 0;
+
+}
+
